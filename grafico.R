0154adcb7a4e68c75838f6c8c83915b621675fad
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2020-07-23 10:14:05 -0300

    faz gráfico

---
+++ grafico.R
@@ -0,0 +1,7 @@
+arquivo<-read.csv(file="./saidas/erros.csv",header=TRUE, sep=",");
+
+erros <-as.numeric(as.character(arquivo$erro));
+epocas <-as.numeric(as.character(arquivo$epoca));
+
+plot(epocas,erros);
+lines(lowess(epocas,erros));
